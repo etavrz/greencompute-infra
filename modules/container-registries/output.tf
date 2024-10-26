6b2689ce233eee0f69f53a09c8c21cf80d506d76
Elias Tavarez <etav@umich.edu>
2024-10-26 14:52:51 -0500

    fix: add output for ecrs

--- modules/container-registries/output.tf
+++ modules/container-registries/output.tf
@@ -1,4 +1,14 @@
-output "ecr_instace" {
-  value = aws_ecr_repository.greencompute_backend.id
+output "ecr_backend" {
+  value = aws_ecr_repository.greencompute_backend.repository_url
+  description = "The URL of the ECR repository"
+}
+
+output "ecr_frontend" {
+  value = aws_ecr_repository.greencompute_frontend.repository_url
+  description = "The URL of the ECR repository"
+}
+
+output "ecr_webserver" {
+  value = aws_ecr_repository.greencompute_webserver.repository_url
   description = "The URL of the ECR repository"
 }
--- output.tf
+++ output.tf
@@ -3,12 +3,22 @@ output "iam_user" {
   description = "The ID of the IAM user"
 }
 
-output "ecr_instance" {
-  value = module.ecr.ecr_instace
-  description = "The URL of the ECR repository"
-}
-
 output "vpc_id" {
   value = module.network.vpc_id
   description = "The ID of the VPC"
+}
+
+output "ecr_backend" {
+  value = module.ecr.ecr_backend
+  description = "The URL of the ECR repository"
+}
+
+output "ecr_frontend" {
+  value = module.ecr.ecr_frontend
+  description = "The URL of the ECR repository"
+}
+
+output "ecr_webserver" {
+  value = module.ecr.ecr_webserver
+  description = "The URL of the ECR repository"
 }
